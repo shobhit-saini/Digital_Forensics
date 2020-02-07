628ddc70be5587906c1575909c20619904cf8f81
SHOBHIT <shobhitsaini95@gmail.com>
2020-02-07 16:04:33 +0530

    Update code.py

--- Assignment4/code.py
+++ Assignment4/code.py
@@ -4,41 +4,67 @@ Created on Wed Feb  5 22:37:17 2020
 
 @author: SHOBHIT SAINI
 """
+
 import cv2
 import numpy as np
-cap = cv2.VideoCapture('D:\IIT_BBS_CLG_WORK\IITBBS_LAB_WORK\DF2_lab\Assignment_4\Train002.avi')
+from sklearn import preprocessing
+from skimage import feature, io
+from scipy.spatial import distance
+video1 = cv2.VideoCapture("D:\IIT_BBS_CLG_WORK\IITBBS_LAB_WORK\DF2_lab\Assignment_4\Train002.avi")
+video2 = cv2.VideoCapture("D:\IIT_BBS_CLG_WORK\IITBBS_LAB_WORK\DF2_lab\Assignment_4\Train003\.avi")
+#Driver function       
+def opticalflow(cap): 
+    #Reading first frame and applying laplacian filter
+    ret, frame1 = cap.read()
+    frame_arr = np.zeros_like(frame1)
+    frame1=cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY) 
+    pre=np.uint8(cv2.normalize(cv2.Laplacian(frame1,cv2.CV_64F),None,0,255,cv2.NORM_MINMAX))
+    count = 0    
+    frame_arr[...,1] = 255
+    fm=[]
+    while(1):
+        ret, frame2=cap.read()
+        if ret == False:
+                break  
+        frame2=cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY) 
+        nxt=np.uint8(cv2.normalize(cv2.Laplacian(frame2,cv2.CV_64F),None,0,255,cv2.NORM_MINMAX))
+        flow =cv2.calcOpticalFlowFarneback(pre,nxt, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+        #cartToPolar used to compute the magnitude and angle of 2D vectors
+        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+        ang=ang*180/np.pi/2
+        mx=(np.floor(np.max(ang))/3)+1
+        frame_arr[...,0] = ang
+        #normalize the magnitute 
+        frame_arr[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+        # 3*3 array initialize to zeros
+        ar = np.zeros((3,3),dtype=int)
+        for i in range(frame_arr.shape[0]):
+            for j in range(frame_arr.shape[1]):
+                frame_arr[i,j,0]=frame_arr[i,j,0]/mx
+                frame_arr[i,j,2]=frame_arr[i,j,2]/86
+                ar[frame_arr[i,j,0],frame_arr[i,j,2]] += 1
+        ar=ar.reshape([1,9])
+        fm.append(ar)
+        pre = nxt
+        count+=1
+        if count==20:
+            break
+    fm=np.asarray(fm) 
+    fm=fm.reshape([len(fm),9]).T
+    return fm
+def mahalanobisdistance(fm1,fm2):
+    mean1=np.mean(fm1,axis=1)  
+    mean2=np.mean(fm2,axis=1)  
+    print(mean1,mean2)   
+    cov1= np.cov(fm1,y=None,rowvar=True,bias=False,ddof=None,fweights=None,aweights=None)
+    covinv=np.linalg.inv(cov1)
+    d=distance.mahalanobis(mean2,mean1,covinv)  
+    print("distance is:=",d)    
 
+fm1=opticalflow(video1)
+fm2=opticalflow(video2)
 
-# Check if camera opened successfully
-if (cap.isOpened()== False): 
-  print("Error opening video stream or file")
- 
-# Read until video is completed
-while(cap.isOpened()):
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  if ret == True:
-    # Convert the image to grayscale
-    src_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Apply Laplace function
-    dst = cv2.Laplacian(src_gray, cv2.CV_64F)
-    
-    # converting back to uint8
-    abs_dst = cv2.convertScaleAbs(dst)
-    # Display the resulting frame
-    cv2.imshow('Frame',abs_dst)
- 
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-      break
- 
-  # Break the loop
-  else: 
-    break
- 
-# When everything done, release the video capture object
-cap.release()
- 
-# Closes all the frames
+mahalanobisdistance(fm1,fm2)
+video1.release()
+video2.release()
 cv2.destroyAllWindows()
